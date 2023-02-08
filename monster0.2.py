0f4d3e933d8e6ce3083bc0982c60f96db746dd2c
Davey <123515157+DaveyCodeReal@users.noreply.github.com>
2023-02-08 14:43:27 -0800

    monsters

---
+++ monster0.2.py
@@ -0,0 +1,173 @@
+import random
+
+# version 0.2
+
+
+print ('this monster is a', end=' ')
+
+# generation starts
+
+monsterDesc1 = [
+'replilian', 
+'kinda cute',
+'big n\' hairy',
+'built Ford tough',
+'very ugly',
+'rather pleasant',
+'large and dangerous',
+'Stephen King\'s',
+'big nasty',
+'kinda pathetic',
+'cat-sized but dangerous',
+'Costco-sized',
+'quite poised',
+'undersized', 
+'bus-sized',
+'skyscraper-sized',
+'terrifyingly powerful',
+'enormous']
+print(random.choice(monsterDesc1), end=' ')
+
+monsterDesc2 = [
+'beast',
+'kaiju',
+'dragon',
+'generic forest cryptid',
+'gorilla-esque beast',
+'snake',
+'bigfoot',
+'jackalope',
+'squid type thing',
+'yeti',
+'sea goat',
+'water bull',
+'kelpie',
+'ghost whale',
+'ant-lion',
+'wraith',
+'cartoon mummy',
+'ghoul',
+'banshee',
+'trickster spider',
+'scorpion',
+'moth man',
+'half-puma-man',
+'zombie gnome',
+'harpy',
+'Fenghuang',
+'Chamrosh',
+'Vegetable Lamb of Tartary',
+'Gagana',
+'Yatagarasu',
+'Zhenniao',
+'Basilisk',
+'slime',
+'anthropomorphic bison',
+'anthropomorphic wolf',
+'anthropomorphic squid',
+'anthropomorphic great dane',
+'anthropomorphic lion',
+'demon']
+print(random.choice(monsterDesc2))
+
+
+
+print ('It has', end=' ')
+
+monsterEyes = [
+'enormous bloodshot eyes',
+'one eye with an expertly applied smoky look',
+'tiny beedy eyes, like a doll\'s eyes',
+'big green anime eyes',
+'dark eyes that give a withering look',
+'beautiful dark eyes',
+'blue eyes like limpid pools of tears',
+'little yellow reptilian eyes']
+print(random.choice(monsterEyes), end=' ')
+               
+print ('and', end=' ')
+
+monsterEyes = [
+'5 tiny mouths',
+'a jaw that unhinges to reveal another, even scarier mouth',
+'big pouty lips',
+'a Glasgow smile',
+'a big gross mouth',
+'a rather unscary mouth with a few cavities',
+'teeth that are so straight and white it\'s scary',
+'a regular mouth',
+'a proboscus like a mosquito', 
+'a big be-fanged mouth',
+'a fairly normal mouth but rancid breath']
+print(random.choice(monsterEyes))
+
+# more or just print?
+monsterDescPref = [
+'It has',
+'It\'s got']
+print(random.choice(monsterDescPref), end=' ')
+
+# limbs or fur
+monsterDesc3 = [
+'a great many tentacles',
+'a body like a linebacker (sweaty and overworked)',
+'a thick wintery pelt',
+'long blond hair like a Pantene commercial',
+'a single tentacle that looks unwashed',
+'huge feet with steel-toed boots',
+'big flappers',
+'fins',
+'huge hands',
+'cinematicly blood-dripping fangs',
+'prehensile braids',
+'a few prehensile tails',
+'an lure on its head that looks like a limited edition Funko Pop',
+'an lure on its head that looks like a Spiderman comic',
+'an lure on its head that looks like a Twix bar',
+'an lure on its head that looks like a pack of smokes',
+'an lure on its head that looks like a 2TB SSD',
+'an lure on its head that looks like a chocolate muffin',
+'long red fur']                
+print(random.choice(monsterDesc3), end=' ')
+
+print ('and', end=' ')
+                
+# additional appearance or quality
+monsterDesc4 = [
+'poor depth perception',
+'fleas',
+'a tragic backstory',
+'a nasty temper',
+'ill-fitting pants.',
+'a gun??',
+'a big stick',
+'a bad attitide.',
+'a broken bottle.',
+'a book of riddles',
+'very bad breath.',
+'a EULA',
+'limited liability insurance.',
+'unpatched vulnerabilities.',
+'a passive-agressive attitude.',
+'a penchant for gaslighting.',
+'toxic traits out the wazoo.',
+'a big rolled up newspaper.',
+'bad jokes.',
+'stans JKR.',
+'ticks.',
+'rabies.',
+'giant claws',
+'muddy knee-high boots.',
+'knife-like claws',
+'huge weird feet',
+'a forlorn demeanor', 
+'an SUV']                
+print(random.choice(monsterDesc4), end=' ')
+               
+
+
+
+
+
+
+
